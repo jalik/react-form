f0f509ccd98983acbe9b56e54f90bf262b00e2a8
Karl Stein <karl.stein.pro@gmail.com>
2020-08-06 12:58:50 -1000

    Returns the form submit result as `submitResult` in `useForm()`

--- src/useForm.js
+++ src/useForm.js
@@ -344,7 +344,7 @@ function useForm(
       throw new Error('onSubmit must return a Promise');
     }
     return promise.then((result) => {
-      dispatch({ type: ACTION_SUBMITTED });
+      dispatch({ type: ACTION_SUBMITTED, data: { result } });
       return result;
     }).catch((error) => {
       dispatch({ type: ACTION_SUBMIT_ERROR, error });
@@ -510,6 +510,7 @@ function useForm(
     modifiedClass,
     submitCount: state.submitCount,
     submitError: state.submitError,
+    submitResult: state.submitResult,
     submitted: state.submitted,
     submitting: state.submitting,
     validClass,
