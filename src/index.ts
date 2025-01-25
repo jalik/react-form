0e57a541ee43b40dcc43c3daf214c00cc1612296
Karl Stein <karl.stein.pro@gmail.com>
2025-01-24 14:11:21 -1000

    test: add export tests for package index

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,34 @@
 /*
  * This file is licensed under the MIT License (MIT)
- * Copyright (c) 2024 Karl STEIN
+ * Copyright (c) 2025 Karl STEIN
  */
 
-import Button from './components/Button'
-import Field from './components/Field'
-import FieldError from './components/FieldError'
-import Form from './components/Form'
-import Option from './components/Option'
-
-import useFieldArray from './useFieldArray'
-import useForm from './useForm'
-import useFormContext from './useFormContext'
-
-import { getFieldId, getFieldValue, inputValue } from './utils'
-
+// export functions
 export {
-  Button,
-  Field,
-  FieldError,
-  Form,
-  Option,
+  build,
+  flatten,
   getFieldId,
   getFieldValue,
+  getIndexFromPath,
+  hasDefinedValues,
+  hasTrueValues,
   inputValue,
-  useFieldArray,
-  useForm,
-  useFormContext
-}
+  movePathIndices,
+  randomKey,
+  reconstruct,
+  resolve,
+  swapPathIndices,
+  updatePathIndices
+} from './utils'
+
+// export components
+export { default as Button, ButtonProps } from './components/Button'
+export { default as Field, FieldProps } from './components/Field'
+export { default as FieldError, FieldErrorProps } from './components/FieldError'
+export { default as Form, FormProps } from './components/Form'
+export { default as Option, OptionProps } from './components/Option'
+
+// export hooks
+export { default as useFieldArray, UseFieldArrayOptions } from './useFieldArray'
+export { default as useForm, UseFormOptions, UseFormHook } from './useForm'
+export { default as useFormContext, FormContext } from './useFormContext'
---
+++ test/index.test.ts
@@ -0,0 +1,45 @@
+/*
+ * This file is licensed under the MIT License (MIT)
+ * Copyright (c) 2025 Karl STEIN
+ */
+
+import { describe, expect, it } from '@jest/globals'
+import * as index from '../src/index'
+
+describe('index', () => {
+  it('should export components', () => {
+    expect(index.Button).toBeDefined()
+    expect(index.Field).toBeDefined()
+    expect(index.FieldError).toBeDefined()
+    expect(index.Form).toBeDefined()
+    expect(index.Option).toBeDefined()
+  })
+
+  it('should export contexts', () => {
+    expect(index.FormContext).toBeDefined()
+  })
+
+  it('should export hooks', () => {
+    expect(index.useFieldArray).toBeDefined()
+    expect(index.useForm).toBeDefined()
+    expect(index.useFormContext).toBeDefined()
+  })
+
+  it('should export functions', () => {
+    expect(index.build).toBeDefined()
+    expect(index.flatten).toBeDefined()
+    expect(index.hasDefinedValues).toBeDefined()
+    expect(index.hasTrueValues).toBeDefined()
+    expect(index.reconstruct).toBeDefined()
+    expect(index.resolve).toBeDefined()
+
+    expect(index.getFieldId).toBeDefined()
+    expect(index.inputValue).toBeDefined()
+    expect(index.randomKey).toBeDefined()
+
+    expect(index.getIndexFromPath).toBeDefined()
+    expect(index.movePathIndices).toBeDefined()
+    expect(index.swapPathIndices).toBeDefined()
+    expect(index.updatePathIndices).toBeDefined()
+  })
+})
