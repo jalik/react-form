ef9d2224052338dff4d9e6e14378fba60f7750d2
Karl Stein <karl.stein.pro@gmail.com>
2025-04-01 14:23:39 -1000

    test: add tests for `useForm().onSuccess()`

---
+++ test/useForm/onSuccess.test.ts
@@ -0,0 +1,69 @@
+/*
+ * This file is licensed under the MIT License (MIT)
+ * Copyright (c) 2025 Karl STEIN
+ */
+
+import useForm from '../../src/useForm'
+import { describe, expect, it, jest } from '@jest/globals'
+import { act, renderHook } from '@testing-library/react'
+import { FormMode } from '../../src'
+
+function tests (mode: FormMode) {
+  const initialValues = {
+    a: 1,
+    b: 2
+  }
+  describe('when submit is successful', () => {
+    it('should call onSuccess function', async () => {
+      const onSuccess = jest.fn()
+      const hook = renderHook(() => useForm({
+        mode,
+        initialValues,
+        onSubmit: () => Promise.resolve({ success: true }),
+        onSuccess
+      }))
+      await act(() => hook.result.current.submit())
+      expect(onSuccess).toHaveBeenCalledTimes(1)
+    })
+
+    it('should call onSuccess function with result and values', async () => {
+      let callbackArgs: unknown[] = []
+      const onSuccess = jest.fn((...args) => {
+        callbackArgs = args
+      })
+      const result = { success: true }
+      const hook = renderHook(() => useForm({
+        mode,
+        initialValues,
+        onSubmit: () => Promise.resolve(result),
+        onSuccess
+      }))
+      await act(() => hook.result.current.submit())
+      expect(callbackArgs.length).toBe(2)
+      expect(callbackArgs[0]).toStrictEqual(result)
+      expect(callbackArgs[1]).toStrictEqual(hook.result.current.getValues())
+    })
+  })
+
+  describe('when submit failed', () => {
+    it('should not call onSuccess function', async () => {
+      const onSuccess = jest.fn()
+      const hook = renderHook(() => useForm({
+        mode,
+        initialValues,
+        onSubmit: () => Promise.reject(new Error('failed')),
+        onSuccess
+      }))
+      await act(() => hook.result.current.submit())
+      expect(onSuccess).toHaveBeenCalledTimes(0)
+    })
+  })
+}
+
+describe('useForm({ mode: "controlled", onError: function }).submit()', () => {
+  tests('controlled')
+})
+
+describe('useForm({ mode: "uncontrolled", onError: function }).submit()', () => {
+  tests('uncontrolled')
+})
